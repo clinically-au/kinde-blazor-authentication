7eec1ada3ac0873604aadb6d989eee107caa3a27
Wojt Janowski <wojt@clinically.com.au>
2024-03-29 15:00:49 +1100

    MInor changes, nuget package settings etc.

--- Kinde.Authentication/AuthorizationPermissionPolicyBuilder.cs
+++ Kinde.Authentication/AuthorizationPermissionPolicyBuilder.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using Kinde.ManagementApi.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 
---
+++ Kinde.ManagementApi/README.md
@@ -0,0 +1,3 @@
+Just a basic client generated with OpenApi Generator for the Kinde API
+
+Referenced by other libraries that need it
